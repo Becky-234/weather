2ba41daa2a7f29e776aa32b0a04068b48e9563d2
Becky-234 <kiraborebecca04@gmail.com>
2025-09-02 16:11:29 +0300

    Math round on temperature

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log('Welcome to weather');
+//standard way to configure an express app
+const express = require('express');             //importation
+const app = express();                           //instantation
+const port = 3000;
+
+//route for the landing page
+app.get('/', (req, res) => {
+    res.send('Get started!');
+});
+
+//route for the home page
+app.get('/home', (req, res) => {
+    res.send('Browse your city weather!');
+});
+
+
+//last line in this file
+app.listen(port, () => console.log(`Listening on port ${port}`));
